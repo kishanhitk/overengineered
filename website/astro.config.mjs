f78f0bea2717a11afc6a140e0981680e40b8c3c8
kishanhitk <kishansharma1231@gmail.com>
2024-09-27 23:38:25 +0530

    feat(fe): add node adapter

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
