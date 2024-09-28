807866e6411a0cfffaf5d64b424a51379bbe54f2
kishanhitk <kishansharma1231@gmail.com>
2024-09-28 12:22:36 +0530

    feat(fe): add prefetch

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,7 +6,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-
+  prefetch: true,
   adapter: node({
     mode: "standalone",
   }),
